35e198edd3d8e5c3973bd7c219c97a88524e2cf6
lzyprime <2383518170@qq.com>
2025-03-04 18:09:36 +0800

    更新rootfs下载地址

--- rvck-kernel-build/run.sh
+++ rvck-kernel-build/run.sh
@@ -11,7 +11,7 @@ fi
 repo_name="$(echo "${REPO##h*/}" | awk -F'.' '{print $1}')"
 kernel_result_dir="${repo_name}_pr_${ISSUE_ID}"
 download_server=10.213.6.54
-rootfs_download_url=https://repo.tarsier-infra.isrc.ac.cn/openEuler-RISC-V/RVCK/openEuler24.03-LTS-SP1/openeuler-rootfs.img
+rootfs_download_url='https://mirror.iscas.ac.cn/openeuler-sig-riscv/openEuler-RISC-V/RVCK/openEuler24.03-LTS-SP1/openeuler-rootfs.img'
 kernel_download_url="http://${download_server}/kernel-build-results/${kernel_result_dir}/Image"
 
 # yum install 
